d3788d28bae5ca9d3f59de9cc41317f534c237a7
Thomas <95196470+TFHarrison@users.noreply.github.com>
2024-10-14 18:22:52 +1100

    Update data_conv.py

--- data_conv.py
+++ data_conv.py
@@ -10,21 +10,25 @@ with open('Main_data.csv', 'r') as f:
 years={}
 stations={}
 
-for lin in data:
+for lin in data[:-1]:
     
     #seperating out data
-    loc=','.join(lin[:2])
-    year=lin[2]
-    month=lin[3]
-    rain=float(lin[4])
+    loc=','.join(lin[:3])
+    year=lin[3]
+    month=lin[4]
+    rain=float(lin[5])
 
     #seperating out data for stations
     if loc in stations:
         if year in stations[loc]:
-            stations[loc][year][0]+=rain
-            stations[loc][year][1]+=1
-        else:stations[loc][year]=[rain,1]
-    else:stations[loc]={year:[rain,1]}
+            stations[loc][year][0][0]+=rain
+            stations[loc][year][0][1]+=1
+            if rain>stations[loc][year][1]:
+                stations[loc][year][1]=rain
+            elif rain<stations[loc][year][1]:
+                stations[loc][year][1]=rain
+        else:stations[loc][year]=[[rain,1],rain,rain]
+    else:stations[loc]={year:[[rain,1],rain,rain]}
 
     #Seperating out per year
     if year in years:
@@ -33,13 +37,17 @@ for lin in data:
             years[year][month][1]+=1
         else:years[year][month]=[rain, 1]
     else:years[year]={month:[rain,1]}
-
+    
 #Cleaning the statiion data to give 1 value per station
-for station in stations:
-    sdat=stations[station]
-    ydat=[sdat[year][0]/sdat[year][1] for year in sdat]
-    stations[station]=sum(ydat)/len(ydat)
-
+with open('Clean/Station_data.csv', 'w') as f:
+    f.write('Name,Lat,Long,Year,Mean,Max,Min\n')
+    for station in stations:
+        sdat=stations[station]
+        for year in sdat:
+            f.write(f'{station},{year},{round(sdat[year][0][0]/sdat[year][0][1],2)},{sdat[year][1]},{sdat[year][2]}\n')
+        ydat=[sdat[year][0][0]/sdat[year][0][1] for year in sdat]
+        stations[station]=[round(sum(ydat)/len(ydat),2), sdat[max(sdat, key=lambda x: sdat[x][1])][1],sdat[min(sdat, key=lambda x: sdat[x][2])][2]]
+        
 #default final lists
 months_f={'01':[0,0], '02':[0,0], '03':[0,0], '04':[0,0], '05':[0,0], '06':[0,0],
           '07':[0,0], '08':[0,0], '09':[0,0], '10':[0,0], '11':[0,0], '12':[0,0]}
@@ -62,40 +70,36 @@ for year in years:
         avg[0]+=mavg
         avg[1]+=1
         
-    years_f[year]=yavg[0]/yavg[1]
+    years_f[year]=round(yavg[0]/yavg[1],2)
     
-avg=avg[0]/avg[1]
+avg=round(avg[0]/avg[1],2)
 
 for month in months_f:
     months_f[month]=months_f[month][0]/months_f[month][1]
 
 #Writing to all the files
-with open('Webpg/Clean/Station_avg.csv', 'w') as f:
-    f.write('Lat,long,Rain\n')
+with open('Clean/Station_avg.csv', 'w') as f:
+    f.write('Name,Lat,Long,Mean,Max,Min\n')
     for station in stations:
-        f.write(station+','+str(stations[station])+'\n')
+        f.write(station+','+','.join([str(i) for i in stations[station]])+'\n')
 
-with open('Webpg/Clean/Monthly_avg.csv', 'w') as f:
+with open('Clean/Monthly_avg.csv', 'w') as f:
     f.write('Month,Rain\n')
     for month in months_f:
         f.write(month+','+str(months_f[month])+'\n')
 
-with open('Webpg/Clean/Yearly_avg.csv', 'w') as f:
+with open('Clean/Yearly_avg.csv', 'w') as f:
     f.write('Year,Rain\n')
     for year in sorted(years_f):
         f.write(year+','+str(years_f[year])+'\n')
 
-with open('Webpg/Clean/Station_dev.csv', 'w') as f:
-    f.write('Lat,long,Rain\n')
+with open('Clean/Station_dev.csv', 'w') as f:
+    f.write('Name,Lat,Long,Rain\n')
     for station in stations:
-        f.write(station+','+str(stations[station]-avg)+'\n')
+        f.write(station+','+str(stations[station][0]-avg)+'\n')
 
-with open('Webpg/Clean/Monthly_dev.csv', 'w') as f:
-    f.write('Month,Rain\n')
-    for month in months_f:
-        f.write(month+','+str(months_f[month]-avg)+'\n')
-
-with open('Webpg/Clean/Yearly_dev.csv', 'w') as f:
+with open('Clean/Yearly_dev.csv', 'w') as f:
     f.write('Year,Rain\n')
     for year in sorted(years_f):
         f.write(year+','+str(years_f[year]-avg)+'\n')
+
